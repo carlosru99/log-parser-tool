a175be1f65ddad40f33f45ab5a2b04cd68626b43
Carlos Ruiz <cruiz@nextdigital.es>
2024-05-28 20:56:06 +0200

    ✨ feat: add main call to log data parser

---
+++ main_data_parser.py
@@ -0,0 +1,74 @@
+import datetime
+import sys
+
+def parse_log(
+            file_path: str,
+            time_init: datetime.datetime,
+            time_end: datetime.datetime,
+            hostname: str) -> list:
+    """
+    Parses a log file to find connections between hosts in a specified time range
+
+    Args:
+        file_path (str): The path to the log file
+        time_init (datetime): The start time of the time range
+        time_end (datetime): The end time of the time range
+        hostname (str): The hostname to find connections for
+
+    Returns:
+        list: A list of hostnames connected to the target within the specified time range
+    """
+
+    connections = []
+    time_init_ts = int(time_init.timestamp() * 1000)
+    time_end_ts = int(time_end.timestamp() * 1000)
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for log in file:
+            splitted_file = log.strip().split(sep=" ")
+            timestamp = int(splitted_file[0])
+            if time_init_ts <= timestamp <= time_end_ts:
+                host_from = splitted_file[1]
+                host_to = splitted_file[2]
+                if host_from == hostname:
+                    connections.append(host_to)
+
+    return connections
+
+
+def str_to_datetime(date_str: str) -> datetime.datetime:
+    
+    """
+    Convert stringified datetime in '%Y-%m-%d %H:%M:%S' format into datetime .
+
+    Args:
+        date_str (str): datetime in string format.
+
+    Returns:
+        datetime.datetime: A datetime.datetime format value
+    """
+
+    return datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+def main():
+    if len(sys.argv) != 5:
+        print("Uso: python main.py <nombre_archivo> <init_timestamp> <end_timestamp> <hostname>")
+        sys.exit(1)
+
+    try:
+        file_path = sys.argv[1]
+        time_init = str_to_datetime(sys.argv[2])
+        time_end = str_to_datetime(sys.argv[3])
+        hostname = sys.argv[4]
+
+        connections = parse_log(file_path, time_init, time_end, hostname)
+        print(f"Conexiones encontradas para el Host \"{hostname}\":", connections)
+    except ValueError as ve:
+        print(f"Error en los formatos de fecha/hora: {ve}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
